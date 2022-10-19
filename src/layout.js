8832d662c6f9278199679b3e008363c09a0a67e1
ImomushiRan <george.villarroel@outlook.es>
2022-10-18 23:34:35 -0300

    Fix functions, add factory function

--- src/layout.js
+++ src/layout.js
@@ -12,8 +12,10 @@ export function section(){
     const section = document.createElement('section');
     section.classList.add('container');
 
-    return section;
+    section.appendChild(navbar());
+    section.appendChild(main());
 
+    return section;
 }
 
 export function navbar(){
@@ -26,14 +28,34 @@ export function navbar(){
     p.innerHTML = 'Home';
     const separation = document.createElement('div');
     separation.classList.add('separation');
-
+    
     div.appendChild(separation);
     div.appendChild(p);
     nav.appendChild(div);
-
+    
     return nav;
 }
 
 export function main(){
+    const main = createElement('main')
 
+    const div = createElement('div', 'home');
+}
+
+export function div(){
+    const div = createElement('div', 'container');
+
+    div.appendChild(header());
+    div.appendChild(section());
+
+    return div;
+}
+
+
+function createElement(name, cName = name, textCont = ''){
+    const element = document.creteElement(name);
+    element.classList.add(cName);
+    element.innerHTML = textCont;
+
+    return element;
 }
